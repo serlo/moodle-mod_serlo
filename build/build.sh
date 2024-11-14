bef71362241380335313eb6c6ec7c38355c2613b
Macbook <tham.tt@morsoftware.com>
2024-11-14 09:04:40 +0700

    Update build script, always install last version @serlo/editor, @serlo/editor-web-component

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,9 @@
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPTPATH"
 
+echo "Remove package-lock.json (force install)"
+rm -rf package-lock.json
+
 echo "Installing node_modules"
 
 npm install
