1b6170f9b2b14cbaa60f7fb8e9f6482ce1185201
Kai Brobeil <kai@adornis.de>
2025-02-11 18:31:27 +0000

    chore: bump version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025012204; // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2025021101; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2022041900; // Requires this Moodle version.
 $plugin->component = 'mod_serlo'; // Full name of the plugin (used for diagnostics).
-$plugin->release   = '1.1.2';
+$plugin->release   = '1.1.4';
 $plugin->maturity  = MATURITY_STABLE;
